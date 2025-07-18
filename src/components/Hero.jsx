a4b3b1afe24d2ba90cefa56058efdb891bb3d3c6
shaikhsanan04 <sananblog007@gmail.com>
2025-07-18 18:20:52 +0530

    added resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import './Hero.css';
 import tempImg from '../assets/temp.png';
-import resume from 'public/sanan-resume.pdf';
 
 
 const Hero = () => {
@@ -25,7 +24,7 @@ const Hero = () => {
                 View My Projects
               </button>
               <a
-                href={resume}
+                href="/sanan-resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 download="Sanan_Shaikh_Resume.pdf"
